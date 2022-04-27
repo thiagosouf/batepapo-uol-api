b1bd341dcf2d25e26b336bfce185d8d97a21d97b
thiagosouf <thiagosouf@gmail.com>
2022-04-27 12:57:36 -0300

    mensagens aparecendo

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import dayjs from 'dayjs'
 
 const app = express(); // Cria um servidor
 app.use(express.json()); // Permite que o express entenda json
@@ -12,10 +13,16 @@ const participantes = [
     {name: 'João3', lastStatus: 12313123},
 ]
 
+const mensagens =[
+    {from: 'João', to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')},
+    {from: 'João1', to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs().format('HH:mm:ss')}
+]
+
 
 // Configura uma função pra ser executada quando bater um GET na rota "/"
 app.post("/participants", (req, res) => {
     const {name} = req.body;
+    console.log(dayjs().format('HH:mm:ss'));
     if (name === "") {
         console.log("Nome não pode ser vazio");
         res.status(422).send("Nome não informado");
@@ -35,7 +42,31 @@ app.post("/participants", (req, res) => {
 });
 
 
-app.post("")
+app.get("/participants", (req, res) => {
+    res.send(participantes);
+});
+
+app.post("/messages", (req, res) => {
+    const {to, text, type} = req.body;
+    const from = req.headers.user;
+    let destinatario = participantes.find(participante => participante.name === from);
+    if ((to === "") || (text === "") || (type !== "message" && type !== "private_message") || (!destinatario)) {
+        console.log("Dados incompletos");
+        res.status(422).send("Dados incompletos");
+        return
+    }
+
+    mensagens.push({from, to, text, type, time: dayjs().format('HH:mm:ss')});
+    console.log(mensagens);
+    res.sendStatus(201);
+
+})
+
+app.get("/messages", (req, res) => {
+    res.send(mensagens);
+})
+
+
 
 // Configura o servidor para rodar na porta 5000
 app.listen(5000, console.log("Server ligado na porta 5000"));
